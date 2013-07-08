78090f4fb29ab18913fc95992856d9f4536a68f2
Dan Childs <danchildsshop@gmail.com>
2013-07-09 00:09:49 +0100

    clean up screwups from 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 module ApplicationHelper
-<<<<<<< HEAD
-  # Returns the full title on a per-page basis.
-    def full_title(page_title)
-      base_title = "Ruby on Rails Tutorial Sample App"
-      if page_title.empty?
-        base_title
-      else
-        "#{base_title} | #{page_title}"
-      end
-    end
-=======
+
   
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -19,7 +9,6 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
-  
->>>>>>> filling-in-layout
+
   
 end
